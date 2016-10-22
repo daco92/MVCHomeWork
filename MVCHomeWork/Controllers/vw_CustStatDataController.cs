516717c28ccc1ce87a23b348b3ff6530d368d935
Daco <Daco@DESKTOP-TBK340K>
2016-10-22 22:06:13 +0800

    * 實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，此功能要在資料庫中建立一個檢視表，並且匯入 EDMX。 * 主選單要有連結可以直接連到三個主要功能的列表頁。

---
+++ MVCHomeWork/Controllers/vw_CustStatDataController.cs
@@ -0,0 +1,20 @@
+﻿using MVCHomeWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomeWork.Controllers
+{
+    public class vw_CustStatDataController : Controller
+    {
+        private CustomerEntities db = new CustomerEntities();
+
+        // GET: vw_CustStatData
+        public ActionResult Index()
+        {
+            return View(db.vw_CustStatData.ToList());
+        }
+    }
+}
--- MVCHomeWork/Models/Customer.Context.cs
+++ MVCHomeWork/Models/Customer.Context.cs
@@ -30,6 +30,7 @@ namespace MVCHomeWork.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<vw_CustStatData> vw_CustStatData { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
